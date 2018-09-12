5fdf456bf73c73d4cdb76d59eab5c3566da48237
92arek <a.pamulski@gmail.com>
2018-09-12 21:37:00 +0200

    updated code, deleted redundant code

--- src/main/java/programmersSelfstudy/arrays/Exercise4.java
+++ src/main/java/programmersSelfstudy/arrays/Exercise4.java
@@ -5,8 +5,8 @@ import static programmersSelfstudy.arrays.Exercise4.ChessFigures.*;
 public class Exercise4 {
 
     public enum ChessFigures {
-        EMPTY, B_PAWN, B_KING, B_QUEEN, B_ROOK, B_KNIGHT, B_BISHOP,
-        W_PAWN, W_KING, W_QUEEN, W_ROOK, W_KNIGHT, W_BISHOP;
+        EMPTY, B_PAWN, B_QUEEN, B_ROOK, B_KNIGHT,
+        W_PAWN, W_KING, W_ROOK, W_KNIGHT
     }
 
     private int[][] chessBoard = new int[8][8];
@@ -18,9 +18,9 @@ public class Exercise4 {
                 new ChessFigures[]{EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
                 new ChessFigures[]{EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
                 new ChessFigures[]{EMPTY, B_QUEEN, B_PAWN, EMPTY, EMPTY, W_KNIGHT, EMPTY, EMPTY},
-                new ChessFigures[]{},
-                new ChessFigures[]{},
-                new ChessFigures[]{},
+                new ChessFigures[]{EMPTY, B_KNIGHT, EMPTY, EMPTY, W_ROOK, EMPTY, W_PAWN, EMPTY},
+                new ChessFigures[]{EMPTY, EMPTY, EMPTY, B_ROOK, EMPTY, EMPTY, EMPTY, EMPTY},
+                new ChessFigures[]{EMPTY, W_KING, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY},
         };
     }
 }
