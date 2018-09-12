8700c3dce6ebc15a831bef6fbea4f2719aa8524e
92arek <a.pamulski@gmail.com>
2018-09-12 20:12:18 +0200

    updated code

--- src/main/java/programmersSelfstudy/arrays/ChessFigures.java
+++ src/main/java/programmersSelfstudy/arrays/ChessFigures.java
@@ -1,5 +1,5 @@
 package programmersSelfstudy.arrays;
 
 public enum ChessFigures {
-    PAWN, KING, QUEEN, ROOK, KNIGHT, BISHOP, EMPTY
+  B_PAWN, B_KING, B_QUEEN, B_ROOK, B_KNIGHT, B_BISHOP, W_PAWN, W_KING, W_QUEEN, W_ROOK, W_KNIGHT, W_BISHOP;
 }
--- src/main/java/programmersSelfstudy/arrays/Exercise4.java
+++ src/main/java/programmersSelfstudy/arrays/Exercise4.java
@@ -1,5 +1,18 @@
 package programmersSelfstudy.arrays;
 
 public class Exercise4 {
-    
+    private int[][] chessBoard = new int[8][8];
+
+    public static int[][] boardState() {
+        return new int[][]{
+                new int[]{},
+                new int[]{},
+                new int[]{},
+                new int[]{},
+                new int[]{},
+                new int[]{},
+                new int[]{},
+                new int[]{},
+        };
+    }
 }
