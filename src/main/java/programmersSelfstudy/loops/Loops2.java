20d7885f41ccc4e9a144b74b3836c5e661db10fb
92arek <a.pamulski@gmail.com>
2018-09-13 11:00:04 +0200

    simple while loop

---
+++ src/main/java/programmersSelfstudy/loops/Loops2.java
@@ -0,0 +1,15 @@
+package programmersSelfstudy.loops;
+
+//Napisz pętlę while, która wypisze na ekranie wszystkie liczby od 10 do 20 włącznie.
+
+public class Loops2 {
+
+    public static void main(String[] args) {
+        int number = 10;
+        while (number <= 20){
+            System.out.println(number);
+            number++;
+        }
+    }
+
+}
