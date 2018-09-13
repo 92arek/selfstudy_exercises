4abc50a73f2eba43ba3e0fcdf92a17d9582cf7a7
92arek <a.pamulski@gmail.com>
2018-09-13 14:19:29 +0200

    completed the task

--- src/main/java/programmersSelfstudy/loops/Loops6.java
+++ src/main/java/programmersSelfstudy/loops/Loops6.java
@@ -1,4 +1,19 @@
 package programmersSelfstudy.loops;
 
 public class Loops6 {
+
+    private static int sum(int[][] table) {
+        int i = 0;
+        for (int[] ints : table) {
+            for (int anInt : ints) {
+                i +=anInt;
+            }
+        }
+        return i;
+    }
+
+    public static void main(String[] args) {
+        int[][] table = new int[][]{new int[]{1,5,3},new int[]{1,1,2,8,6}};
+        System.out.println(sum(table));
+    }
 }
