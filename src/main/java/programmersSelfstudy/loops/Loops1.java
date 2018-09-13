1c05e7e670dee7f7f6c2bdb301fbdc5ba4e7e627
92arek <a.pamulski@gmail.com>
2018-09-13 11:00:48 +0200

    added exercise comment

--- src/main/java/programmersSelfstudy/loops/Loops1.java
+++ src/main/java/programmersSelfstudy/loops/Loops1.java
@@ -1,5 +1,7 @@
 package programmersSelfstudy.loops;
 
+//Napisz program, który wypisze na ekranie malejąco wszystkie liczby od 20 do 10.
+
 public class Loops1 {
 
     public static void main(String[] args) {
