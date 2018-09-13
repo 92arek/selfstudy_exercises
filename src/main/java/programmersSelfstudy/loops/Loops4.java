14ec04f6ca316e1cb33d76e1baa414a712e419ff
92arek <a.pamulski@gmail.com>
2018-09-13 12:47:32 +0200

    done the exercise

--- src/main/java/programmersSelfstudy/loops/Loops4.java
+++ src/main/java/programmersSelfstudy/loops/Loops4.java
@@ -3,4 +3,13 @@ package programmersSelfstudy.loops;
 //Przerób pętlę z zadania trzeciego na pętlę while.
 
 public class Loops4 {
+
+    public static void main(String[] args) {
+        int i = -10;
+        while (i <= 40) {
+            if (i % 2 != 0)
+                System.out.println(i);
+            i++;
+        }
+    }
 }
