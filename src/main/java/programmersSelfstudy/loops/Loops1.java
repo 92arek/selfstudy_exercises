c1467db07d1c1c819d9eb0fe40b64ec98b6864f0
92arek <a.pamulski@gmail.com>
2018-09-13 10:55:08 +0200

    simple loop

---
+++ src/main/java/programmersSelfstudy/loops/Loops1.java
@@ -0,0 +1,9 @@
+package programmersSelfstudy.loops;
+
+public class Loops1 {
+
+    public static void main(String[] args) {
+        for (int i = 20; i >=10 ; i--)
+            System.out.println(i);
+    }
+}
