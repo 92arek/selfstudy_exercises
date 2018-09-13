7ecdd0af710e55bc38cdf9b70fcab203eeb41c04
92arek <a.pamulski@gmail.com>
2018-09-13 14:09:10 +0200

    completed the task

--- src/main/java/programmersSelfstudy/loops/Loops5.java
+++ src/main/java/programmersSelfstudy/loops/Loops5.java
@@ -2,4 +2,19 @@ package programmersSelfstudy.loops;
 
 public class Loops5 {
 
+
+    private static int sum(int[] table) {
+        int i = 0;
+        for (int i1 : table) {
+            i++;
+        }
+        return i;
+    }
+
+    public static void main(String[] args) {
+        int[] table = new int[]{1, 5, 4, 67, 8, 9};
+        System.out.println(sum(table));
+    }
+
+
 }
