e1864a7420b3949f79bfa64c96f4582aa0691105
92arek <a.pamulski@gmail.com>
2018-09-13 14:34:03 +0200

    added task requirements

---
+++ src/main/java/programmersSelfstudy/primitives/Average.java
@@ -0,0 +1,8 @@
+package programmersSelfstudy.primitives;
+
+//Napisz program, który policzy średnią z kilku przedmiotów. Możemy założyć, że uczeń w szkole ma 3 przedmioty i z każdego z nich dostał po 4 oceny.
+
+public class Average {
+
+
+}
