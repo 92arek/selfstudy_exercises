b5afeb3648b790668d6a0b853d8ea0ae5656fc06
92arek <a.pamulski@gmail.com>
2018-09-14 11:07:06 +0200

    updated the class

--- src/main/java/programmersSelfstudy/interfaces/TheApp.java
+++ src/main/java/programmersSelfstudy/interfaces/TheApp.java
@@ -1,6 +1,11 @@
 package programmersSelfstudy.interfaces;
 
+import java.util.Scanner;
+
 public class TheApp {
+
+    private Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
         TheApp theApp = new TheApp();
         Computation computation;
@@ -19,10 +24,12 @@ public class TheApp {
     }
 
     private boolean shouldMultiply(){
-        return false;
+        System.out.println("Do you want to Add or Multiply?");
+        return scanner.next().equals("m");
     }
 
     private double getArgument(){
-        return 0;
+        System.out.println("Enter the number: ");
+        return scanner.nextDouble();
     }
 }
