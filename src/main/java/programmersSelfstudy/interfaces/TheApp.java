3a6b84b835c6f64e163be2045212482873273635
92arek <a.pamulski@gmail.com>
2018-09-14 10:48:11 +0200

    updated TheApp class with exercise code

--- src/main/java/programmersSelfstudy/interfaces/TheApp.java
+++ src/main/java/programmersSelfstudy/interfaces/TheApp.java
@@ -1,4 +1,28 @@
 package programmersSelfstudy.interfaces;
 
 public class TheApp {
+    public static void main(String[] args) {
+        TheApp theApp = new TheApp();
+        Computation computation;
+
+        if (theApp.shouldMultiply()){
+            computation = new Multiply();
+        } else {
+            computation = new Add();
+        }
+
+        double argument1 = theApp.getArgument();
+        double argument2 = theApp.getArgument();
+
+        double result = computation.compute(argument1, argument2);
+        System.out.println("Result: " + result);
+    }
+
+    private boolean shouldMultiply(){
+        return false;
+    }
+
+    private double getArgument(){
+        return 0;
+    }
 }
